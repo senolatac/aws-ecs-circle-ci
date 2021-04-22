a75ce790830d73f88fc4776cc4fddf7a5ddcb68d
Senol Atac <senol.atac.1@gmail.com>
2021-04-22 15:53:44 +0300

    initial

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ ADD ${DEPENDENCY}/libs/aws-ecs-circle-ci-0.0.1-SNAPSHOT.jar /var/circleci-with-s
 
 EXPOSE 9999
 
-ENTRYPOINT ["java", "--server.port=80", "-jar","/var/circleci-with-springboot/circleciwithspringboot.jar"]
+ENTRYPOINT ["java", "-Dserver.port=80", "-jar","/var/circleci-with-springboot/circleciwithspringboot.jar"]
--- src/main/java/com/example/awsecscircleci/controller/TestController.java
+++ src/main/java/com/example/awsecscircleci/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.awsecscircleci.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController
 {
+
+    @Value("${custom.ecs.variable}")
+    private int customEcsVariable;
+
     @GetMapping("/test")
     public String testMethod() {
         return "test method";
@@ -23,6 +28,6 @@ public class TestController
 
     @GetMapping("/test-2")
     public String test2Method() {
-        return "test-2 method";
+        return "test-2 method " + customEcsVariable;
     }
 }
